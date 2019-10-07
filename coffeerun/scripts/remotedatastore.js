ea18f5126668d75b4688ee45ce2ec1f559d8efdd
oO-Blink-Oo <alvarezian23@gmail.com>
2019-10-06 20:23:28 -0700

    Utilized Deployd and Mongo

--- coffeerun/scripts/remotedatastore.js
+++ coffeerun/scripts/remotedatastore.js
@@ -7,26 +7,25 @@
     if (!url) {
       throw new Error("No remote URL supplied.");
     }
-
     this.serverUrl = url;
   }
 
   RemoteDataStore.prototype.add = function(key, val) {
-    $.post(this.serverUrl, val, function (serverResponse) {
+    $.post(this.serverUrl, val, function(serverResponse) {
       /* eslint-disable-next-line no-console */
       console.log(serverResponse);
     });
   };
 
-  RemoteDataStore.prototype.getAll = function (cb) {
-    $.get(this.serverUrl, function (serverResponse) {
+  RemoteDataStore.prototype.getAll = function(cb) {
+    $.get(this.serverUrl, function(serverResponse) {
       /* eslint-disable-next-line no-console */
       console.log(serverResponse);
-      cb(serverResponse);
+      cb;
     });
   };
 
-  RemoteDataStore.prototype.get = function (key, cb) {
+  RemoteDataStore.prototype.get = function(key, cb) {
     $.get(this.serverUrl + "/" + key, function(serverResponse) {
       /* eslint-disable-next-line no-console */
       console.log(serverResponse);
@@ -34,7 +33,7 @@
     });
   };
 
-  RemoteDataStore.prototype.remove = function (key) {
+  RemoteDataStore.prototype.remove = function(key) {
     $.ajax(this.serverUrl + "/" + key, {
       type: "DELETE"
     });
@@ -42,5 +41,4 @@
 
   App.RemoteDataStore = RemoteDataStore;
   window.App = App;
-
 })(window);
